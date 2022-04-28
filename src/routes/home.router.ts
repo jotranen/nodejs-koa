77f95b3bf17de92e9d64b3aa717bf484365c924c
Jari Otranen <jari.otranen@gmail.com>
2022-04-28 16:01:53 +0300

    some typescript configs

--- src/routes/home.router.ts
+++ src/routes/home.router.ts
@@ -1,16 +1,21 @@
-const Router = require('@koa/router');
+import Koa from 'koa'
+
+import Router from '@koa/router'
 
 const router = new Router({
     prefix: '/home'
 });
 
-router.get('/', (ctx:any) => {
+router.get('/', (ctx: Koa.ParameterizedContext) => {
     ctx.body = 'Hello home';
 })
 
-router.post('/', (ctx:any) => {
+router.post('/', (ctx: Koa.ParameterizedContext) => {
     ctx.body = 'Hello home - post';
 })
 
 module.exports = router;
 
+function kala(kala: Number) {
+    console.log(kala);
+}
